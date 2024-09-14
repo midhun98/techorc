e98d264a33aa080febc86e4c803c0cdaaab8f93e
Midhun <midhunskani@gmail.com>
2024-09-14 15:10:54 +0530

    TEC-6--APIs for user registration

--- core/views.py
+++ core/views.py
@@ -1,9 +1,27 @@
+from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import RegisterSerializer
 
 
 @api_view()
 @permission_classes([IsAuthenticated])
 def secret(request):
     return Response({"message": "Some secret message"})
+
+
+class RegisterUserAPIView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        if User.objects.filter(username=username).exists():
+            return Response({"message": "Username already exists"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
