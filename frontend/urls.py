620bdac7e901ee125ddff23ae3e546289cf173b4
Midhun <midhunskani@gmail.com>
2024-09-14 16:11:25 +0530

    TEC-8--Create the home page

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("", TemplateView.as_view(template_name="register.html"), name="register"),
     path("login/", TemplateView.as_view(template_name="login.html"), name="login"),
+    path("secret/", TemplateView.as_view(template_name="secret.html"), name="secret"),
 ]
